c8ae7813d7b74a86d3969b654d576bb986e517fb
NoobDude231 <117944326+NoobDude231@users.noreply.github.com>
2022-11-12 02:30:22 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function OpenSidebar(){
+    var x = document.getElementsByClassName("sidebar");
+    x[0].style.display = "inline-block";
+}
+
+function CloseSidebar(){
+    var y = document.getElementsByClassName("sidebar")
+    y[0].style.display = "none";
+}
