01ebabe00a2f53a58dbc4d5c78ae2fff565fc9c9
NoobDude231 <117944326+NoobDude231@users.noreply.github.com>
2022-11-28 21:28:21 +0100

    Delete js directory

--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function OpenSidebar(){
-    var x = document.getElementsByClassName("sidebar");
-    x[0].style.display = "inline-block";
-}
-
-function CloseSidebar(){
-    var y = document.getElementsByClassName("sidebar")
-    y[0].style.display = "none";
-}
